4111958c72a04483cfe26ff47726f4fb513de58f
villegasc <villegasc@gmail.com>
2021-04-11 16:52:52 +0200

    Add controller methods

--- CreditApplications/Controllers/CreditApplicationController.cs
+++ CreditApplications/Controllers/CreditApplicationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,25 @@ namespace CreditApplications.Controllers
     [Route("[controller]")]
     public class CreditApplicationController : ControllerBase
     {
+        private IConfiguration _Config;
+        private CreditApprovalRules _CreditApprovalRules;
+
+        public CreditApplicationController(IConfiguration Configuration)
+        {
+            _Config = Configuration;
+        }
+
+        [HttpGet("version")]
+        public string Get()
+        {
+            return string.Format("Running credit app version {0}",
+                    typeof(CreditApplication).Assembly.GetName().Version.ToString());
+        }
+
+        [HttpPost("evaluate")]
+        public CreditDecision EvaluateCreditApplication([FromBody] CreditApplication creditApplication)
+        {
+            return new CreditDecision(creditApplication, _CreditApprovalRules);
+        }
     }
 }
--- CreditApplications/CreditDecision.cs
+++ CreditApplications/CreditDecision.cs
@@ -10,5 +10,18 @@ namespace CreditApplications
         public bool IsApproved { get; set; }
         public double InterestRate { get; set; }
         private CreditApplication _CApplication { get; set; }
+        private CreditApprovalRules _CApprovalRules { get; set; }
+
+        public CreditDecision(CreditApplication cApplication, CreditApprovalRules aRules) 
+        {
+            _CApplication = cApplication;
+            _CApprovalRules = aRules;
+            ProcessRequest();
+        }
+
+        private void ProcessRequest()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
