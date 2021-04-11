1696267f13328b947db82091ea2b064b6d46ae23
villegasc <villegasc@gmail.com>
2021-04-11 17:00:11 +0200

    Add sorted dictionary to appoval rules

--- CreditApplications/CreditApprovalRules.cs
+++ CreditApplications/CreditApprovalRules.cs
@@ -9,6 +9,22 @@ namespace CreditApplications
     {
         public double MinLoanAmount { get; set; }
         public double MaxLoanAmount { get; set; }
-        public Dictionary<double, double> CurrentInterestRates { get; set; }
+        public Dictionary<string, string> CurrentInterestRates { get; set; }
+
+        private SortedDictionary<double, double> _CurrentInterestRatesParsed = new SortedDictionary<double, double>();
+
+        public SortedDictionary<double, double> GetCurrentInterestRates()
+        {
+            if (CurrentInterestRates == null)
+            {
+                return null;
+            }
+            foreach (KeyValuePair<string, string> rate in CurrentInterestRates)
+            {
+                _CurrentInterestRatesParsed.Add(double.Parse(rate.Key), double.Parse(rate.Value));
+            }
+            return _CurrentInterestRatesParsed;
+        }
     }
+
 }
